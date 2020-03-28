17517e05034deecd28054ec8e32b06ea4b50aeed
Matt Sinfield <matt_sinfield@hotmail.com>
2020-03-28 16:47:15 +0000

    initial commit

---
+++ src/Server.js
@@ -0,0 +1,41 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+const http = require('http');
+const moment = require('moment');
+
+const manager = {
+    name: 'bos',
+    age: 37,
+};
+
+const empl1 = {
+    name: 'Feed Blogs',
+    age: 28,
+};
+
+const empl2 = {
+    name: 'Doe Vasile',
+    age: 34,
+};
+
+const textHeader = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/plain', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' };
+const jsonHeader = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept' };
+
+http.createServer((req, res) => {
+    const { url } = req;
+    console.log(`called page ${url} at moment: ${moment().format()}`);
+    switch (url) {
+        case '/managers':
+            res.writeHead(200, jsonHeader);
+            res.end(JSON.stringify(manager));
+            break;
+        case '/employees':
+            res.writeHead(200, jsonHeader);
+            res.end(JSON.stringify([empl1, empl2]));
+            break;
+        default:
+            res.writeHead(404, textHeader);
+            res.end('Page not found!');
+    }
+    res.writeHead(200, textHeader);
+}).listen(9090);
